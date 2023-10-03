e7ab0a7ff1aeafd1139fe34a06f71a848032625a
Thiyan Arumugam <thiyan@wso2.com>
2023-10-03 09:52:12 +0530

    Add github banner

--- src/components/Common/GithubBanner.tsx
+++ src/components/Common/GithubBanner.tsx
@@ -12,27 +12,81 @@ function GithubBanner({ content, marginTop = 3, marginBottom = 2 }: Props) {
   const heading = "HEY! WANT TO KNOW HOW IT IS BUILT?";
   const linkText = "CHECK OUT OUR CODE REPOSITORIES >>";
   return (
-    <Box
-      bgcolor={alpha("#00A79D", 0.75)}
-      alignItems="center"
-      justifyContent="center"
-      padding={5}
-      marginTop={marginTop}
-      marginBottom={marginBottom}
-    >
-      <Grid container alignItems="center" justifyContent="center" padding={1}>
-        <Stack spacing={3}>
-          <Typography variant="h2" align="center" color="#00255C" maxWidth="lg">
-            {heading}
-          </Typography>
-          <Typography variant="h5" align="center" color="#00255C" maxWidth="lg">
-            <Link href="https://google.com" underline="none" target="_blank">
-              {linkText}
-            </Link>
-          </Typography>
-        </Stack>
-      </Grid>
-    </Box>
+    <>
+      <Box
+        sx={{
+          // backgroundImage: "url('github.png')",
+          // backgroundBlendMode: "multiply",
+          position: "relative",
+          minHeight: 200,
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <Box
+          sx={{
+            backgroundColor: "#00A79D60",
+            // backdropFilter: "blur(5px)",
+            flexDirection: "row",
+            display: "flex",
+            minHeight: 200,
+          }}
+        >
+          <Grid
+            container
+            alignItems="center"
+            justifyContent="center"
+            padding={1}
+            spacing={5}
+          >
+            <Grid
+              container
+              item
+              lg={3}
+              alignItems="center"
+              justifyContent="center"
+            >
+              <img src="github.png" alt="github" width="100" />
+            </Grid>
+            <Grid item lg={6}>
+              <Stack spacing={3}>
+                <Typography
+                  variant="h2"
+                  align="center"
+                  color="#00255C"
+                  maxWidth="lg"
+                >
+                  {heading}
+                </Typography>
+                <Typography
+                  variant="h5"
+                  align="center"
+                  color="#00255C"
+                  maxWidth="lg"
+                >
+                  <Link
+                    href="https://google.com"
+                    underline="none"
+                    target="_blank"
+                  >
+                    {linkText}
+                  </Link>
+                </Typography>
+              </Stack>
+            </Grid>
+            <Grid
+              container
+              item
+              lg={3}
+              alignItems="center"
+              justifyContent="center"
+            >
+              <img src="github.png" alt="github" width="100" />
+            </Grid>
+          </Grid>
+        </Box>
+      </Box>
+    </>
   );
 }
 
